abcaea100306bd226e2130b21b18498a066f8e89
Meowmerry <meow3525@gmail.com>
2025-07-03 14:59:29 -0500

    fixed new contribute

--- src/components/PresentationComponents/Contribute/Form/EditExistingVoyage.tsx
+++ src/components/PresentationComponents/Contribute/Form/EditExistingVoyage.tsx
@@ -45,6 +45,7 @@ const EditExistingVoyage: React.FC<EditExistingVoyageProps> = ({
     if (voyageId) {
       setLoading(true);
       const res = await fetchSubmitEditVoaygesForm(voyageId);
+      console.log({ res });
       if (res.status === 200) {
         setEntity(res.data);
         setChangeSet({
@@ -65,7 +66,7 @@ const EditExistingVoyage: React.FC<EditExistingVoyageProps> = ({
       setLoading(true);
     }
   };
-
+  console.log({ id: entity.entityRef });
   const hasEntity = entity && entity.entityRef.id !== 0;
 
   return (
--- src/fetch/contributeFetch/fetchSubmitEditVoaygesForm.ts
+++ src/fetch/contributeFetch/fetchSubmitEditVoaygesForm.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 import { AUTHTOKEN, BASEURLNODE } from '@/share/AUTH_BASEURL';
-// const res = await fetch(
-//   `http://localhost:7127/materialize/Voyage/${voyageId}`,
-// );
+
 export const fetchSubmitEditVoaygesForm = async (voyageId: string) => {
   const response = await axios.get(
     `${BASEURLNODE}/materialize/Voyage/${voyageId}`,
